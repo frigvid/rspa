1639e18b0231241e466fe3ab23d2a030bb7ff6a4
Mike Ropen <67565373+frigvid@users.noreply.github.com>
2023-09-28 10:29:48 +0200

    feat: Initial commit.
    Set license to MIT.
    Added .gitignore templates.
    Added IntelliJ IDEA project base.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.frigvid.rspa {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.frigvid.rspa to javafx.fxml;
+    exports com.frigvid.rspa;
+}
